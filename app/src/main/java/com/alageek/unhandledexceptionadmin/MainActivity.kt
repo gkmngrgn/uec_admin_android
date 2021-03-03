1234cdfcb86ed914e55848715791931b7353781f
Gökmen Görgen <gkmngrgn@gmail.com>
2021-03-04 00:02:23 +0100

    add three buttons in main activity.

--- app/src/main/java/com/alageek/unhandledexceptionadmin/MainActivity.kt
+++ app/src/main/java/com/alageek/unhandledexceptionadmin/MainActivity.kt
@@ -3,16 +3,55 @@ package com.alageek.unhandledexceptionadmin
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Android")
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ButtonTime()
+                Spacer(modifier = Modifier.size(16.dp))
+                ButtonDescription()
+                Spacer(modifier = Modifier.size(16.dp))
+                ButtonLink()
+                Greeting(name = "Android")
+            }
         }
+    }
+}
+
+@Composable
+fun ButtonTime() {
+    Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+        Text(text = stringResource(id = R.string.button_add_time))
+    }
+}
+
+@Composable
+fun ButtonDescription() {
+    Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+        Text(text = stringResource(id = R.string.button_add_description))
+    }
+}
+
+@Composable
+fun ButtonLink() {
+    Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+        Text(text = stringResource(id = R.string.button_add_links))
     }
 }
 
